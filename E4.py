f3d259f96f3d4e391708abeccfa2e65c5ef86c27
Max Latoche <max.latoche@gmail.com>
2022-05-29 23:46:58 -0500

    E4

---
+++ E4.py
@@ -0,0 +1,19 @@
+
+print('E4.12a\n', True)
+print('E4.12b\n', True)
+print('E4.12c\n', True)
+print('E4.12d\n', False)
+print('E4.12e\n', True)
+
+A = [
+    []
+]
+print('E4.13A\n', 'rank', 0,'\nrow space', 0, '\ncolumn space', 0, '\nnullspace', 0, '\n')
+
+B = [
+
+]
+
+C = [
+
+]
