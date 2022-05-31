8c1ed107c74f5eb34324ccca4ac6617355ecd177
Max Latoche <max.latoche@gmail.com>
2022-05-31 00:20:11 +0000

    start strang

---
+++ strang/problem_set_1.py
@@ -0,0 +1,7 @@
+
+
+print('Section 1.2 | Problem 23\n', 'w1/||w||, w2/||w||')
+
+print('Section 1.2 | Problem 28\n', 'Yes.  Think of the 4 quadrants of a plane.  Because vectors need to point in different directions to have negative dot products, 4 vectors in 4 quadrants would be a 0 dot product at least.  But with only three vectors, we just need to pick 3 spaced appropriately.  If you look at the unit circle you can pick (0, 1), (-1/2, √3/2), (-√3/2, -1/2), or any 3 vectors roughly spaced the same amount.')
+
+print('Section 1.3 | Problem 4\n', '')
