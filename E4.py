7b5ed114330b9f12f2ca2e67ab7a043e1838d94c
Max Latoche <max.latoche@gmail.com>
2022-06-08 00:20:11 +0000

    E4

--- E4.py
+++ E4.py
@@ -6,14 +6,30 @@ print('E4.12d\n', False)
 print('E4.12e\n', True)
 
 A = [
-    []
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 1],
 ]
-print('E4.13A\n', 'rank', 0,'\nrow space', 0, '\ncolumn space', 0, '\nnullspace', 0, '\n')
+
+# all answers for 4.13 refer to dimensions
+# nullity(M) == dim(nullspace(M))
+
+# A**mxn === A**3x4
+# to calculate null space, rank(A) + nullity(A) = n
+# to calculate left null space dim(rowspace(A)) + nullity(A) = n
+print('E4.13A\n', 'rank', 3,'\nrow space', 3, '\ncolumn space', 3, '\nnull space', 1,'\nleft null space', 0, '\n')
 
 B = [
-
+    [1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0],
 ]
+print('E4.13B\n', 'rank', 3,'\nrow space', 3, '\ncolumn space', 3, '\nnullspace', 2,'\nleft null space', 0, '\n')
 
 C = [
-
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
 ]
+print('E4.13C\n', 'rank', 0,'\nrow space', 0, '\ncolumn space', 0, '\nnullspace', 0,'\nleft null space', 0, '\n')
