c7f9a4982e12ba8221f3b530fb938185f4321863
Max Latoche <max.latoche@gmail.com>
2022-06-14 00:11:21 +0000

    numpy_practice, circle

--- numpy_practice.py
+++ numpy_practice.py
@@ -8,7 +8,7 @@ slope2, y_intercept_2 = (3/4), 0.0
 
 l = np.linspace(-6, 6, 100)
 
-# plt.figure(figsize=(8, 8))
+plt.figure(figsize=(8, 8))
 
 plt.xlim(0, 6)
 plt.ylim(0, 6)
@@ -20,6 +20,14 @@ x_point_of_intersection = (y_intercept_2 - y_intercept_1) / (slope1 - slope2)
 y_point_of_intersection = slope1 * x_point_of_intersection + y_intercept_1
 
 plt.scatter(x_point_of_intersection, y_point_of_intersection, color='midnightblue')
+
+theta = np.linspace(0, 2*np.pi, 100)
+r = 1.0
+
+x_center = r * np.cos(theta) + x_point_of_intersection
+y_center = r * np.sin(theta) + y_point_of_intersection
+
+plt.plot(x_center, y_center, color='green')
 
 plt.show()
 
