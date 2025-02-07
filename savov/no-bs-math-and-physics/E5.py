869e539395a6e5ca2c6b67fae8a281389c15a941
Max Latoche <max.latoche@gmail.com>
2025-02-07 00:44:09 -0500

    E5.py

---
+++ savov/no-bs-math-and-physics/E5.py
@@ -0,0 +1,9 @@
+
+
+print('E5.1a\n', 0)
+
+# x - dx/dt - 1
+# x^2 - dx/dt - 2x^1
+# remember to drop constants when calculating derivatives (for L'Hopital rule)
+print('E5.1b\n', '2/1 == 2')
+print('E5.1c\n', '2x/1 == ∞')
