9059f12c1e2e3d971dd18347e3df8ede7b351cd0
Max Latoche <max.latoche@gmail.com>
2025-02-11 21:00:43 -0500

    E5 -> P5

--- savov/no-bs-math-and-physics/P5.py
+++ savov/no-bs-math-and-physics/P5.py
@@ -1,56 +1,56 @@
 
-print('E5.1 1\n', -6)
-print('E5.1 2\n', 2)
-print('E5.1 3\n', None)
-print('E5.1 4\n', 8.5)
-print('E5.1 5\n', -5)
-print('E5.1 6\n', None)
-print('E5.1 7\n', -2)
-print('E5.1 8\n', -2)
-print('E5.1 9\n', -2)
-print('E5.1 10\n', False)
-print('E5.1 11\n', "['-10', -5), [-5, 2), [2, 5), (5,10]")
+print('P5.1 1\n', -6)
+print('P5.1 2\n', 2)
+print('P5.1 3\n', None)
+print('P5.1 4\n', 8.5)
+print('P5.1 5\n', -5)
+print('P5.1 6\n', None)
+print('P5.1 7\n', -2)
+print('P5.1 8\n', -2)
+print('P5.1 9\n', -2)
+print('P5.1 10\n', False)
+print('P5.1 11\n', "['-10', -5), [-5, 2), [2, 5), (5,10]")
 
 
-print('E5.2 a\n', 4)
-print('E5.2 b\n', 6)
-print('E5.2 c\n', 5)
+print('P5.2 a\n', 4)
+print('P5.2 b\n', 6)
+print('P5.2 c\n', 5)
 
 # y = x^13
-print('E5.7 1\n', '13x^12')
+print('P5.7 1\n', '13x^12')
 # y = x^-3/2
-print('E5.7 2\n', '(-3/2)x^-5/2')
+print('P5.7 2\n', '(-3/2)x^-5/2')
 # y = x^2a
-print('E5.7 3\n', '2ax^(2a-1)')
+print('P5.7 3\n', '2ax^(2a-1)')
 # u = x^2.4
-print('E5.7 4\n', '2.4x^1.4')
+print('P5.7 4\n', '2.4x^1.4')
 # z = cube_root(x)
-print('E5.7 5\n', '(1/3)x^-2/3')
+print('P5.7 5\n', '(1/3)x^-2/3')
 # y = cube_root(x^-5)
 # chain rule f'(g(x))g'(x)
 # f'(x) === (1/3)x^-2/3
 # f'(g(x)) === (1/3)(x^-5)^-2/3 === (1/3)x^10/3
 # g'(x) === -5x^-6
 # (1/3)x^10/3 * -5x^-6
-print('E5.7 6\n', '(-5/3)x^-8/3')
+print('P5.7 6\n', '(-5/3)x^-8/3')
 # u = 5_root(1/x^8)
-print('E5.7 7\n', '')
+print('P5.7 7\n', '')
 # y = 2x^a
-print('E5.7 8\n', '')
+print('P5.7 8\n', '')
 # y = q_root(x^3)
-print('E5.7 9\n', '')
+print('P5.7 9\n', '')
 
 # y = ax^3 + 6
-print('E5.8 1\n', "y' = 3ax^2")
+print('P5.8 1\n', "y' = 3ax^2")
 # y = 13x^(3/2) - c
 # y' = 13(3/2)x^1/2
-print('E5.8 2\n', "y' = 19.5x^1/2")
+print('P5.8 2\n', "y' = 19.5x^1/2")
 # y = 12x^(1/2) + c^1/2
 # y' = 12((1/2)x^-1/2)
-print('E5.8 3\n', "6x^-1/2")
+print('P5.8 3\n', "6x^-1/2")
 # y = c^(1/2)x^(1/2)
-print('E5.8 4\n', "")
+print('P5.8 4\n', "")
 # u = (az^n - 1)/c
-print('E5.8 5\n', "")
+print('P5.8 5\n', "")
 # y = 1.18t^2 + 22.4
-print('E5.8 6\n', "")
+print('P5.8 6\n', "")
