11e1500da9447f8351d5c27afd734b2d68201600
Max Latoche <max.latoche@gmail.com>
2025-02-11 20:08:51 -0500

    P5.py

---
+++ savov/no-bs-math-and-physics/P5.py
@@ -0,0 +1,41 @@
+
+print('E5.1 1\n', -6)
+print('E5.1 2\n', 2)
+print('E5.1 3\n', None)
+print('E5.1 4\n', 8.5)
+print('E5.1 5\n', -5)
+print('E5.1 6\n', None)
+print('E5.1 7\n', -2)
+print('E5.1 8\n', -2)
+print('E5.1 9\n', -2)
+print('E5.1 10\n', False)
+print('E5.1 11\n', "['-10', -5), [-5, 2), [2, 5), (5,10]")
+
+
+print('E5.2 a\n', 4)
+print('E5.2 b\n', 6)
+print('E5.2 c\n', 5)
+
+# y = x^13
+print('E5.7 1\n', '13x^12')
+# y = x^-3/2
+print('E5.7 2\n', '(-3/2)x^-5/2')
+# y = x^2a
+print('E5.7 3\n', '2ax^(2a-1)')
+# u = x^2.4
+print('E5.7 4\n', '2.4x^1.4')
+# z = cube_root(x)
+print('E5.7 5\n', '(1/3)x^-2/3')
+# y = cube_root(x^-5)
+# chain rule f'(g(x))g'(x)
+# f'(x) === (1/3)x^-2/3
+# f'(g(x)) === (1/3)(x^-5)^-2/3 === (1/3)x^10/3
+# g'(x) === -5x^-6
+# (1/3)x^10/3 * -5x^-6
+print('E5.7 6\n', '(-5/3)x^-8/3')
+# u = 5_root(1/x^8)
+print('E5.7 7\n', '')
+# y = 2x^a
+print('E5.7 8\n', '')
+# y = q_root(x^3)
+print('E5.7 9\n', '')
